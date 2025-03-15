575e5eff13f151cf5dbdc52230092ae876195805
suthakaranburaj <suthakaranburaj@gmail.com>
2025-03-15 23:47:35 +0530

    ////

--- src/controller/User/userController.js
+++ src/controller/User/userController.js
@@ -194,7 +194,9 @@ export const get_current_user = asyncHandler(async (req, res) => {
         'user_amount.is_current':1,
         'user_amount.user_id':user.user_id,
     })
-    user.amount = amount.amount;
+    if(amount){
+        user.amount = amount.amount;
+    }
     // console.log(user)
     return sendResponse(res, true, user, "User detials fetched successfully!");
 });
